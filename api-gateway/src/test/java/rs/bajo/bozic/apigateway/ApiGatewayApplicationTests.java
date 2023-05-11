68967be5bc1814da144908fcb740c32d9aba51a7
Savo Bozic <bajobozic@gmail.com>
2023-05-11 13:50:32 +0200

    Add api gateway module

---
+++ api-gateway/src/test/java/rs/bajo/bozic/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.bajo.bozic.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
