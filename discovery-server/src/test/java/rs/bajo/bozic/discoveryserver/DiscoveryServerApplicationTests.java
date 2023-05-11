bed083f635e4efa331e8c8bd55ae2c586e939e44
Savo Bozic <bajobozic@gmail.com>
2023-05-11 13:34:31 +0200

    Add discovery server module

---
+++ discovery-server/src/test/java/rs/bajo/bozic/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.bajo.bozic.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
